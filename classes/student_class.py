459073ed24e9b0ed0a120bc6832dbcc39b8dcc28
Nick-Lemy <n.kayiramga@alustudent.com>
2024-07-16 17:00:43 +0200

    GPA calculation

--- classes/student_class.py
+++ classes/student_class.py
@@ -3,16 +3,16 @@ class Student:
     def __init__(self, email, names, courses_registered, gpa=0):
         self.email = email
         self.names = names
-        self.courses_registered
+        self.courses_registered = courses_registered
         self.gpa = gpa
-    def calculate_gpa(self, courses_registered):
+    def calculate_gpa(self):
         grades = []
-        for i in courses_registered:
+        for i in self.courses_registered:
             a = int(input(f"{i}= "))
             grades.append(a)
         for i in grades:
             self.gpa += i
         return self.gpa
 
-nick = Student("n.kayiranga@gmail.com", "Nick-Lemy Kayiranga", ["Learning Process", "Self Directed Learning", "Python Programming"])
+nick = Student("n.kayiranga@gmail.com", "Nick-Lemy Kayiranga", courses_registered=["Learning Process", "Self Directed Learning", "Python Programming"])
 print(nick.calculate_gpa())
