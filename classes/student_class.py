aa08c446bf250b72a5ef3d4dbe055a6f391dd715
Nick-Lemy <n.kayiramga@alustudent.com>
2024-07-16 16:52:43 +0200

    Test and first commit

---
+++ classes/student_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Student:
+    def __init__(self, email, names, courses_registered, gpa=0):
+        self.email = email
+        self.names = names
+        self.courses_registered
+        self.gpa = gpa
+    def calculate_gpa(self, courses_registered):
+        grades = []
+        for i in courses_registered:
+            a = int(input(f"{i}= "))
+            grades.append(a)
+        for i in grades:
+            self.gpa += i
+        return self.gpa
+
+nick = Student("n.kayiranga@gmail.com", "Nick-Lemy Kayiranga", ["Learning Process", "Self Directed Learning", "Python Programming"])
+print(nick.calculate_gpa())
